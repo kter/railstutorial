6e9e477fc25cf60b68e36f5bbbf8937be0684a91
kter <me@kter.jp>
2015-06-13 20:29:45 +0900

    ページタイトルのテストを追加した

--- sample_app/spec/requests/static_pages_spec.rb
+++ sample_app/spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
 
@@ -17,6 +22,11 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 
   describe "About page" do
@@ -25,5 +35,10 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+  end
+
+  it "should have the right title" do
+    visit '/static_pages/about'
+    expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
   end
 end
