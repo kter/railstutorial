25f2d8b12380e6a6f80c71378dffb276600b5455
kter <me@kter.jp>
2015-07-11 22:01:10 +0900

    サインアップしたらサインインするようにした

--- sample_app/spec/requests/user_pages_spec.rb
+++ sample_app/spec/requests/user_pages_spec.rb
@@ -61,6 +61,15 @@ describe "User Pages" do
         it { should have_title("Example User") }
         it { should have_content("Welcome to the Sample App") }
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) {User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
     end
   end
 end
